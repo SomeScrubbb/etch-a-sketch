b2d5b1eb5a12414a629fb0c33805b2fc4b2d035c
SomeScrubbb <beepcreeper@gmail.com>
2021-09-03 14:00:38 -0400

    fixed a bug where the etch a sketch would crash upon pressing clear, also made it so clear gets rid of the colors of the squares everytime

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ function enableShading() {
     });
 }
 
+enableShading();
 
 //if you want to make an eraser, do the same thing but with remove
 
@@ -35,7 +36,7 @@ output.textContent = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 let gridDimensions;
-let newSquareCount;
+let newSquareCount = 256;
 slider.oninput = function() {
     output.textContent = this.value;
     //set gridDimensions to the slider value
@@ -43,36 +44,44 @@ slider.oninput = function() {
     newSquareCount = Math.pow(gridDimensions, 2);
 }
 
-console.log(newSquareCount)
 
 let currentSquareCount = 256;
+
 
 let clearButton = document.querySelector('#clear-button');
 
 //create event listener for clear. set it to onclick 
 clearButton.addEventListener('click', (e) => {
     grid.style.gridTemplateColumns = `repeat(${gridDimensions}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${gridDimensions}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridDimensions}, 1fr)`; 
     //if there are currently more squares than the new dimensions, remove squares
     if (currentSquareCount > newSquareCount) {
+        console.log(`current square count: ${currentSquareCount}`)
+        console.log(`new square count: ${newSquareCount}`)
         removeSquares();
+        removeColor();
     //if there are currently less squares than we want, add squares
     } else if (currentSquareCount < newSquareCount) {
+        console.log(`current square count: ${currentSquareCount}`)
+        console.log(`new square count: ${newSquareCount}`)
         addSquares();
+        removeColor();
+    } else {
+        removeColor();
     }
     //set the currentSquareCount to the new amount, now that we have added squares
     currentSquareCount = newSquareCount;
     enableShading();
+    /*divList.forEach(item => {
+        item.classList.remove('color');
+    }); */
 });
 
 
 function removeSquares() {
     let toRemove = currentSquareCount - newSquareCount
-    //while toRemove > 0
     while (toRemove > 0) {
-            //remove the lastChild of the grid container
             grid.removeChild(grid.lastElementChild);
-            //iterate toRemove by subtracting one
             toRemove--;
     }
     //set the grid columns and rows to the new value
@@ -84,12 +93,11 @@ function addSquares() {
     //create a variable that tracks the number of squares to add
     //this variable is equal to the specified dimensions minus the original dimensions
     let toAdd = newSquareCount - currentSquareCount;
+    //create a variable called i which is used as the index number for the id atribute
     let i = currentSquareCount + 1;
     while (toAdd > 0) {
-        //create a variable called i which is used as the index number for the id atribute
         const newDivs = document.createElement('div');
         newDivs.classList.add('grid-element');
-        //set an id to div and the current index number
         newDivs.setAttribute('id', `div${i}`);
         grid.appendChild(newDivs);
         i++;
@@ -97,4 +105,11 @@ function addSquares() {
     }
 }
 
+
+function removeColor () {
+    divList = document.querySelectorAll('.grid-element');
+    divList.forEach(item => {
+        item.classList.remove('color');
+    });
+}
 
